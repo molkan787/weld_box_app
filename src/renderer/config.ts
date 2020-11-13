a415283885beed11069d47dbf21f6dec73bc009d
Dahmane Seghier <worw787@gmail.com>
2020-11-13 13:43:25 +0100

    BAD-104 Added envirement variables file

--- src/renderer/config.ts
+++ src/renderer/config.ts
@@ -1,3 +1,2 @@
-export const config = Object.freeze({
-  GENERATE_CODE_URL: 'https://weld-res-xcczm.ondigitalocean.app/api/v1/chart'
-});
+import env from '../env.json';
+export const config = Object.freeze(env);
