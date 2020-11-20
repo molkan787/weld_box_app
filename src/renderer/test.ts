1bb5af17286d4158031acf3438bc5b68c453fbe4
Dahmane Seghier <worw787@gmail.com>
2020-11-20 13:20:21 +0100

    BAD-120 Fixed renderer errors & warnings

--- src/renderer/test.ts
+++ src/renderer/test.ts
@@ -1,7 +1,5 @@
-import { cloneNestedObject } from "./diagram-core/utils"
-import { Menu } from "./modules/menu"
-import { projectsManager } from "./modules/projects-manager";
+// import { projectsManager } from "./modules/projects-manager";
 
-export function test(){
-  // projectsManager.load("C:\\Users\\Dahmane\\Documents\\BoxTree\\foobar.json");
-}
+// export function test(){
+//   projectsManager.load("C:\\Users\\Dahmane\\Documents\\BoxTree\\bar.json");
+// }
